73904f67284c37a74cfb88275412ee00b521fb08
SWillames <sergiowoc@gmail.com>
2021-06-17 10:15:44 -0300

    tests model(employee).

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,8 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates_format_of :email, with: EMAIL_FORMAT
+  validates :email, uniqueness: true
 end
--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :employee do
-    
+    email { Faker::Internet.unique.email }
+    password { 'secret123' }
+    name { Faker::Name.name }
   end
 end
