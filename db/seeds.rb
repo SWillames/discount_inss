5ef589672c63be6820f70f3045ec0353aa247196
SWillames <sergiowoc@gmail.com>
2021-06-18 14:02:03 -0300

    defining db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'ffaker'
+
+20.times do
+  proponent = Proponent.new(
+    full_name: Faker::Name.name,
+    cpf: Faker::IDNumber.brazilian_citizen_number,
+    birth_date: Faker::Date.between(from: '1960-01-01', to: '2021-01-01'),
+    email: Faker::Internet.unique.email,
+    personal_phone: FFaker::PhoneNumberBR.phone_number,
+    reference_phone: FFaker::PhoneNumberBR.phone_number,
+    salary: rand(800..3000.0),
+    contribution: rand(80..300.0),
+  )
+  address = proponent.build_address
+  address.zip_code = (rand(01000000..99999999).to_s)
+  address.public_place = Faker::Address.street_address
+  address.complement = Faker::Address.secondary_address
+  address.city = Faker::Address.city 
+  address.district = "Center" 
+  address.uf = Faker::Address.state_abbr 
+
+  proponent.save!
+end
